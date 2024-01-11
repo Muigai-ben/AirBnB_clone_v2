147a79da18880c769d7b6fed4b6d9f2f5782d17d
Muigai-ben <muigaibenson37@gmail.com>
2024-01-11 11:21:17 -0800

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-""" a module for fabric script script that generates a .tgz archive."""
+"""A module for fabric scripts that generate .tgz archive"""
 from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
     try:
-"""Create the 'versions' folder if it doesn't exist"""
+        # Create the 'versions' folder if it doesn't exist
         local("mkdir -p versions")
 
-"""Generate the current timestamp for the archive name"""
+        # Generate the current timestamp for the archive name
         timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
 
-"""Define the archive name"""
+        # Define the archive name
         archive_name = "web_static_{}.tgz".format(timestamp)
 
-"""Compress the contents of the 'web_static' folder into the archive"""
+        # Compress the contents of the 'web_static' folder into the archive
         local("tar -cvzf versions/{} web_static".format(archive_name))
 
-"""Return the archive path if successfully generated"""
+        # Return the archive path if successfully generated
         return os.path.join("versions", archive_name)
 
     except Exception as e:
-"""Print an error message and return None if an exception occurs"""
+        # Print an error message and return None if an exception occurs
         print("Error packing: {}".format(e))
         return None
+
 
 # Example usage
 archive_path = do_pack()
